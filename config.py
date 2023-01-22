fbae46a1d2b0fb0fdadb00f6225351a5764ff397
Tim Hart <tim@cortinaplatform.com>
2023-01-22 09:00:17 -0500

    clean up cli usage with defaults, and dictionary of agents

--- config.py
+++ config.py
@@ -1,42 +1,28 @@
 import argparse
-
-# from agents/human import HumanAgent
-# from agents/random import RandomAgent
-# from agents/decision_tree import DecisionTreeAgent
-
-# from agents.my_agent import MyAgent
-
 from human_agent import HumanAgent
 from random_agent import RandomAgent
 from decision_tree_agent import DecisionTreeAgent
 
+agents = {
+    "decision_tree": DecisionTreeAgent,
+    "random": RandomAgent,
+    "human": HumanAgent
+}
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Select agents for the game')
     parser.add_argument('--player1', type=str, default='human',
-                        choices=['human', 'random', 'my_agent'], help='Select agent for player 1')
+                        choices=list(agents.keys()), help='Select agent for player 1')
     parser.add_argument('--player2', type=str, default='random',
-                        choices=['human', 'random', 'my_agent'], help='Select agent for player 2')
-
+                        choices=list(agents.keys()), help='Select agent for player 2')
     return parser.parse_args()
 
 
 def select_agents():
     args = parse_args()
-    player1 = args.player1
-    player2 = args.player2
-
-    if player1 == 'human':
-        player1 = Human()
-    elif player1 == 'random':
-        player1 = RandomAgent()
-    # elif player1 == 'my_agent':
-        # player1 = MyAgent()
-
-    if player2 == 'human':
-        player2 = Human()
-    elif player2 == 'random':
-        player2 = RandomAgent()
-    # elif player2 == 'my_agent':
-        # player2 = MyAgent()
-
+    cliPlayerArgs1 = args.player1
+    cliPlayerArgs2 = args.player2
+    player1 = agents[cliPlayerArgs1]()
+    player2 = agents[cliPlayerArgs2]()
     return player1, player2
--- play.py
+++ play.py
@@ -6,7 +6,7 @@ import tkinter as tk
 
 player_1, player_2 = select_agents()
 root = tk.Tk()
-root.title("Tic-Tac-Toe")
+# root.title("Tic-Tac-Toe")
 board = Board(root)
 root.mainloop()
 
